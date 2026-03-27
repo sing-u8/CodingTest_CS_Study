1a6436bf3d5a316b0ff5277b846d959369c0a157
singyupark <singyu13@gmail.com>
2026-03-27 17:37:58 +0900

    fix: 사칙연산 문제 코드 수정 (ops 타입 변경 및 DP 로직 오류 수정)

--- java/src/main/java/platform/programmers/DP/사칙연산/Solution.java
+++ java/src/main/java/platform/programmers/DP/사칙연산/Solution.java
@@ -3,7 +3,7 @@ class Solution {
     public int solution(String arr[]) {
         int n = (arr.length + 1) / 2;
         int[] nums = new int[n];
-        int[] ops = new char[n - 1];
+        char[] ops = new char[n - 1];
         for (int i = 0; i < arr.length; i++) {
             if (i % 2 == 0) nums[i / 2] = Integer.parseInt(arr[i]);
             else ops[i / 2] = arr[i].charAt(0);
@@ -29,10 +29,10 @@ class Solution {
                 int j = i + len - 1;
                 for (int k = i; k < j; k++) {
                     if (ops[k] == '+') {
-                        maxDP[i][j] = Math.max(maxDP[i][j], maxDP[i][k] + max[k+1][j]);
+                        maxDP[i][j] = Math.max(maxDP[i][j], maxDP[i][k] + maxDP[k+1][j]);
                         minDP[i][j] = Math.min(minDP[i][j], minDP[i][k] + minDP[k+1][j]);
                     } else {
-                        maxDP[i][j] = Math.max(maxDP[i][j], maxDP[i][k] - min[k+1][j]);
+                        maxDP[i][j] = Math.max(maxDP[i][j], maxDP[i][k] - minDP[k+1][j]);
                         minDP[i][j] = Math.min(minDP[i][j], minDP[i][k] - maxDP[k+1][j]);
                     }
                 }
